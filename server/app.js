916af199ab6cf998ceaa3ed14af181c93467e844
Faiyaz Rahman <faiyazrahman03@gmail.com>
2021-09-22 00:25:34 +0600

    initial project setup, database connection

---
+++ server/app.js
@@ -0,0 +1,38 @@
+//dependencies
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const path = require('path');
+
+//internal imports
+const { notFoundHandler, defaultErrorHandler } = require('./middlewares/errorHandlers');
+
+//initialise the app
+const app = express();
+
+//initialise cors
+app.use(cors({ origin: true, credentials: true }));
+
+//express body parser
+app.use(express.json());
+
+//set static folder
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('client/build'));
+} else {
+   app.use(express.static(path.join(__dirname, 'public')));
+}
+
+//routers
+//app.use('/api/breeds/', breedsController); //TODO: Create breeds route and controller
+
+app.get('/', (req, res, next) => {
+   res.status(200).json({ status: 'success', message: `This is root of ${process.env.APP_NAME}` });
+});
+
+app.all('*', notFoundHandler);
+
+app.use(defaultErrorHandler);
+
+//export the app
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,18 @@
+//dependencies
+const mongoose = require('mongoose');
+//internal imports
+const app = require('./app');
+
+//database connection
+mongoose
+   .connect(process.env.MONGODB_CONNECTION_STRING)
+   .then(() => {
+      console.log(`App is sucessfully connected to the database`);
+   })
+   .catch((error) => console.log(error));
+
+//configure the port
+const PORT = process.env.NODE_ENV || 5000;
+
+//start the server
+app.listen(PORT, () => console.log(`App is alive on localhost:${PORT}`));
